196e62339ce2a85c50113ed4201a6e11dfabbc55
Sentry11 <99908368+Sentry11@users.noreply.github.com>
2022-11-19 19:50:58 +0200

    Update auth.rotes.js

--- routes/auth.rotes.js
+++ routes/auth.rotes.js
@@ -1,27 +1,17 @@
 const {Router} = require('express')
-
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
 const {check, validationResult} = require('express-validator')
 const jwt = require('jsonwebtoken') 
 const config = require('config')
 
-
 const router = Router()
-
-
 // /api/auth/register
 router.post('/register',
 [ check('email', 'Uncorected email').isEmail(), check('password','min lenght password is 6').isLength({min:6})],
 async (req, res) => {
-
-
-
     try{
-
-    
         const errors = validationResult(req)
-
         if(!errors.isEmpty()){
             return res.status(400).json({
                 errors:errors.array(),
@@ -29,9 +19,7 @@ async (req, res) => {
             })
         }
 
-      
         const {email, password} = req.body
-
         const candidate = await User.findOne({ email })
 
         if (candidate){
@@ -42,7 +30,6 @@ async (req, res) => {
         const user = new User({email, password:hashedPassword})
 
         await user.save()
-
         res.status(201).json({message: 'User registred'})
     }
     catch(e){
@@ -51,30 +38,22 @@ async (req, res) => {
 
 })
 
-
 // /api/auth/login
 router.post('/login',
 [check('email', "Enter correct email").normalizeEmail().isEmail(),
 check('password', 'Enter password').exists()],
  async (req, res) => {
-
     try{
-
         const errors = validationResult(req)
-        
         if(!errors.isEmpty()){
             return res.status(400).json({
                
                 errors:errors.array(),
                 message:'Uncorected authorization data'
             })
-         
         }
 
-        console.log(errors)
-
         const {email,password} = req.body
-
         const user = await User.findOne({email})
 
         if (!user) {
@@ -92,16 +71,11 @@ check('password', 'Enter password').exists()],
             config.get('jwtSecret'),
             {expiresIn:'1h'}
         )
-
         res.json({token, userId: user.id})
-
-
-    
     }
     catch(e){
     res.status(500).json({message:'Something went wrong, try again'})
     }
-
 })
 
 module.exports = router
